a2f7e28cc9dacf29593eb7c87aeb886b43ae903f
anarchitekt <ragan.patres@gmail.com>
2016-03-24 21:21:42 +0100

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,10 +21,19 @@ user2 = User.create! :email => 'patres2@gmail.com', :password => 'aaaaaaaa', :pa
 scrap1 = Scrap.create! city: "Kosice", part_of_town: "KVP", room_count: 3, keywords: "balkon",
                        price_min: 50000, price_max: 80000, last_search_time: DateTime.now, time_of_result_expiration: now
 
+scrap2 = Scrap.create! city: "Kosice", part_of_town: "Terasa", room_count: 2, keywords: "balkon",
+                       price_min: 40000, price_max: 60000, last_search_time: DateTime.yesterday, time_of_result_expiration: yesterday
+
 flat1 = Flat.create! name: "bytik1", content: "toto je concent", price: 90000, date_of_scrap: now,
                      room: 3, date_of_creation: DateTime.now, scrap_id: scrap1.id
 
-flat2 = Flat.create! name: "bytik2", content: "toto je concent", price: 90044, date_of_scrap: yesterday,
+flat2 = Flat.create! name: "bytik2", content: "toto je concent", price: 60000, date_of_scrap: yesterday,
+                     room: 2, date_of_creation: yesterday, scrap_id: scrap2.id
+
+flat3 = Flat.create! name: "bytik2", content: "toto je concent", price: 70000, date_of_scrap: yesterday,
+                     room: 2, date_of_creation: yesterday, scrap_id: scrap2.id
+
+flat4 = Flat.create! name: "bytik2", content: "toto je concent", price: 90044, date_of_scrap: yesterday,
                      room: 2, date_of_creation: yesterday, scrap_id: scrap1.id
 
 comment1 = Comment.create! content: "spokojny", date_of_creation: yesterday, user_id: user1.id, flat_id: flat1.id
